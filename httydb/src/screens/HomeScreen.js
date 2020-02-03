8ef40649f1c22dc6ddda12ddb9d225fe30166a83
4rsalan <arsalan.farooqui2@georgebrown.ca>
2020-02-02 22:21:10 -0500

    Modified list rendering

--- httydb/src/screens/HomeScreen.js
+++ httydb/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, Alert} from 'react-native';
+import {View, Text, Button, Alert, StyleSheet, ScrollView} from 'react-native';
 
 class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -17,9 +17,24 @@ class HomeScreen extends React.Component {
     return (
       <View>
         <Text>Home Screen!</Text>
+        <View>
+          <Text>Recent Layouts</Text>
+        </View>
+        <View>
+          <Text>Recent Analytics</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  ViewStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  }
+});
 
 export default HomeScreen;
