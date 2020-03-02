08e3621504dae35507f09d418ea9c95a2750979f
Jolene Le <renreine4x1@gmail.com>
2020-03-02 13:22:14 -0500

    add fields to AddTeammate
    fixed the teamScreen

--- httydb/src/models/team_member.ts
+++ httydb/src/models/team_member.ts
@@ -1,15 +1,21 @@
 export class Team_member {
-    public fname : string;
-    public lname : string;
-    public email : string;
-    public id : number;
-    public active : boolean;
+  public fname: string;
+  public lname: string;
+  public email: string;
+  public id: number;
+  public active: boolean;
 
-    constructor(fname : string, lname : string, email : string, id : number, active : boolean) {
-        this.id = id;
-        this.lname =lname;
-        this.fname = fname;
-        this.email = email;
-        this.active = active;
-    }
+  constructor(
+    id: number,
+    fname: string,
+    lname: string,
+    email: string,
+    active: boolean,
+  ) {
+    this.id = id;
+    this.lname = lname;
+    this.fname = fname;
+    this.email = email;
+    this.active = active;
+  }
 }
