6653e96361fc342ae40edc87e5e069099e32b535
Jolene Le <renreine4x1@gmail.com>
2020-03-02 13:48:35 -0500

    touched up the database typo

--- httydb/src/db/db_init.tsx
+++ httydb/src/db/db_init.tsx
@@ -1,36 +1,38 @@
-import SQLite from "react-native-sqlite-storage";
+import SQLite from 'react-native-sqlite-storage';
 
 export class db_init {
-    public updateTables(db: SQLite.SQLiteDatabase): Promise<void> {
-        console.log("db_init updateTables");
-        let dbversion: number = 0;
-        return db.transaction(this.createTables).then (() => {
-            return this.getVersion(db)
-        }).then(version => {
-            dbversion = version
-            console.log ("db version: "+ version);
-            //code for updating DB versions go here
-        });
-    }
+  public updateTables(db: SQLite.SQLiteDatabase): Promise<void> {
+    console.log('db_init updateTables');
+    let dbversion: number = 0;
+    return db
+      .transaction(this.createTables)
+      .then(() => {
+        return this.getVersion(db);
+      })
+      .then(version => {
+        dbversion = version;
+        console.log('db version: ' + version);
+        //code for updating DB versions go here
+      });
+  }
 
-    private createTables(transaction: SQLite.Transaction) {
-        console.log("db_init createTables");
-        // team members
-            transaction.executeSql(
-                'CREATE TABLE IF NOT EXISTS team_member( ' + 
-                    '"team_member_id"	INTEGER NOT NULL AUTOINCREMENT, ' +
-                    '"name"	TEXT NOT NULL, '+
-                    '"email"	TEXT NOT NULL, '+
-                    '"phone"	TEXT NOT NULL, '+
-                    '"emergency_cont"   TEXT NOT NULL'+
-                    '"gender"   TEXT NOT NULL'+
-                    '"wieght"   NUMBER NOT NULL'+
-                    '"hieght"   NUMBER NOT NULL'+
-                    '"side_preference" TEXT'+
-                    '"active"   NUMBER'+
-                    'PRIMARY KEY("team_member_id")'+
-                    ');'
-            );/*
+  private createTables(transaction: SQLite.Transaction) {
+    console.log('db_init createTables');
+    // team members
+    transaction.executeSql(
+      'CREATE TABLE IF NOT EXISTS team_member( ' +
+        '"team_member_id"	INTEGER PRIMARY KEY, ' +
+        '"name"	TEXT NOT NULL, ' +
+        '"email"	TEXT NOT NULL, ' +
+        '"phone"	TEXT NOT NULL, ' +
+        '"emergency_cont"   TEXT NOT NULL,' +
+        '"gender"   TEXT NOT NULL,' +
+        '"weight"   NUMBER NOT NULL,' +
+        '"height"   NUMBER NOT NULL,' +
+        '"side_preference" TEXT,' +
+        '"active"   NUMBER' +
+        ');',
+    ); /*
             //paddler
             transaction.executeSql(
                 'CREATE TABLE IF NOT EXISTS paddler( '+
@@ -82,26 +84,29 @@ export class db_init {
                     '"timestamp"    UNSIGNED BIG INT'+
                     'PRIMARY KEY("point_id")'+
                 ');'
-            );       // */     
+            );       // */
 
-            // Version table
-            transaction.executeSql(
-                "CREATE TABLE IF NOT EXISTS Version( " +
-                "version_id INTEGER PRIMARY KEY NOT NULL, " +
-                "version INTEGER" +
-                ");"
-            );
-    }
-    private getVersion(db: SQLite.SQLiteDatabase): Promise<number> {
-        return db.executeSql("SELECT version FROM Version ORDER BY version DESC LIMIT 1;")
-            .then(([results]) => { 
-                if (results.rows && results.rows.length > 0) {
-                    const version = results.rows.item(0).version;
-                    return version;
-                  } else {
-                    return 0;
-                  }
-            }).catch (()=> {return 0 });// do some error handling
-    }
+    // Version table
+    transaction.executeSql(
+      'CREATE TABLE IF NOT EXISTS Version( ' +
+        'version_id INTEGER PRIMARY KEY NOT NULL, ' +
+        'version INTEGER' +
+        ');',
+    );
+  }
+  private getVersion(db: SQLite.SQLiteDatabase): Promise<number> {
+    return db
+      .executeSql('SELECT version FROM Version ORDER BY version DESC LIMIT 1;')
+      .then(([results]) => {
+        if (results.rows && results.rows.length > 0) {
+          const version = results.rows.item(0).version;
+          return version;
+        } else {
+          return 0;
+        }
+      })
+      .catch(() => {
+        return 0;
+      }); // do some error handling
+  }
 }
-
