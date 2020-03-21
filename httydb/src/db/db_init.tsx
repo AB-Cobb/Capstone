0e01a90ca5d39cf9e5a68b5543370513139c0038
Andrew Cobb <andrew@cobbs-web.com>
2020-03-20 22:36:03 -0400

    fixing race table

--- httydb/src/db/db_init.tsx
+++ httydb/src/db/db_init.tsx
@@ -23,6 +23,13 @@ export class db_init {
                 })
               })
             }
+            if (dbversion == 2){
+              db.executeSql('DROP TABLE race').then(()=>{
+                db.transaction(this.createTables).then(() => {
+                  db.executeSql("INSERT INTO version (version) VALUES (3)")
+                })
+              })
+            }
         });
     }
 
