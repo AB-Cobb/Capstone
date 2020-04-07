ec81c7bde506def655a76690036ee6d96c1a71fa
Jolene Le <renreine4x1@gmail.com>
2020-04-07 06:45:51 -0400

    Config simulator on ios

--- httydb/ios/httydb/AppDelegate.m
+++ httydb/ios/httydb/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import GoogleMaps;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCgs8SGQmsKRus2PXjHZ0vLJLVYvu9UZVM"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"httydb"
